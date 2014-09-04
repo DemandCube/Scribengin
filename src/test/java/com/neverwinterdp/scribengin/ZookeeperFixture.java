2400b780e471ac4515222369f03e8a6a7f2c111c
Lih Chen <lihster@gmail.com>
2014-09-04 06:54:05 +0000

    FIXED: wasn't setting the LOG4j env variable.

--- src/test/java/com/neverwinterdp/scribengin/ZookeeperFixture.java
+++ src/test/java/com/neverwinterdp/scribengin/ZookeeperFixture.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
@@ -11,9 +12,12 @@ public class ZookeeperFixture extends Fixture {
   private Process proc;
   private String version;
   private static final String PROPERTIES_FILENAME = "/zookeeper.properties";
+  private static final String LOG4J_FILENAME = "/log4j.properties";
+  private static final String TEMPLATED_LOG4J_FULLPATH = "servers/%s/resources" + LOG4J_FILENAME;
+
   private static final String TEMPLATED_PROPERTIES_FULLPATH = "servers/%s/resources" + PROPERTIES_FILENAME;
   private static final String JAVA_MAIN = "org.apache.zookeeper.server.quorum.QuorumPeerMain";
-  private static final String WAIT_FOR_REGEX = ".*?in standalone mode.*?";
+  private static final String WAIT_FOR_REGEX = ".*?Established session.*?";
 
   public ZookeeperFixture(String version, String host, int port) throws IOException {
     super();
@@ -31,20 +35,28 @@ public class ZookeeperFixture extends Fixture {
     this.renderConfig(
       String.format(TEMPLATED_PROPERTIES_FULLPATH, this.version),
       this.tmpDir.getAbsolutePath() + PROPERTIES_FILENAME,
-      context
-    );
+      context );
+    this.renderConfig(
+      String.format(TEMPLATED_LOG4J_FULLPATH, this.version),
+      this.tmpDir.getAbsolutePath() + LOG4J_FILENAME,
+      context );
 
     ProcessBuilder pb = new ProcessBuilder(
       String.format(KAFKA_RUN_CLASS_SH, this.version),  //"servers/0.8.1/kafka-bin/bin/kafka-run-class.sh",
       JAVA_MAIN,                                        // "org.apache.zookeeper.server.quorum.QuorumPeerMain",
       tmpDir.getAbsolutePath() + PROPERTIES_FILENAME
     );
+    Map<String, String> env = pb.environment();
+    System.out.println(this.tmpDir.getAbsolutePath() + LOG4J_FILENAME);
+    env.put("KAFKA_LOG4J_OPTS", "-Dlog4j.configuration=file:" + this.tmpDir.getAbsolutePath() + LOG4J_FILENAME);
+
     this.proc =  pb.start();
 
-    //block until we see the regex string
     BufferedReader br = new BufferedReader(new InputStreamReader(this.proc.getInputStream()));
     String line = null;
+    //block until we see the regex string
     while ((line = br.readLine()) != null) {
+      System.out.println(line);
       if (line.matches(WAIT_FOR_REGEX)) {
         break;
       }
