4e1a3b756850529f5bc13517cbe6a6e4976e955c
artfullyContrived <anthony.musyoki@gmail.com>
2014-09-23 16:58:09 +0000

    Added marker interfaces for the 3 types of modes.

---
+++ src/main/java/com/neverwinter/scribengin/api/Master.java
@@ -0,0 +1,15 @@
+package com.neverwinter.scribengin.api;
+
+/*
+ * Modes.
+ * 1. Pseudo distrubuted
+ * 2. Yarn Distributed
+ * 3. Standalone Distributed
+ * */
+
+/**
+ * Marker interface for a master that commands workers.
+ * */
+public interface Master {
+
+}
---
+++ src/main/java/com/neverwinter/scribengin/api/Worker.java
@@ -0,0 +1,14 @@
+package com.neverwinter.scribengin.api;
+
+/**
+ * Marker Interface for a worker that is run by a Master.
+ * */
+public interface Worker {
+  /*
+   * Modes.
+   * 1. Pseudo distrubuted
+   * 2. Yarn Distributed
+   * 3. Standalone Distributed
+   * */
+
+}
