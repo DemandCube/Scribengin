ee8053a3693bc0fe18989a579adc9fe97386246f
KingOfPoptart <richard.c.duarte@gmail.com>
2014-10-30 16:54:50 +0000

    Updating run script to be more flexible

--- vagrant/scripts/run.sh
+++ vagrant/scripts/run.sh
@@ -13,8 +13,40 @@ control_c()
 
 usage()
 {
-  echo -e "Usage:\n./run.sh [topic1] [topic2] ... [topicN]"
+  echo -e "Usage:\n./run.sh [options] [topic1] [topic2] ... [topicN]"
+  echo -e "    -k [host:port]  = Host/port for Kafka. Default is [hostname]:9092"
+  echo -e "    -h [host:port]  = Host/port for HDFS. Default is hdfs://[hostname]:8020"
+  echo -e "    -c              = Enable clean start algorithm for ScribeConsumer. Default is disabled"
 }
+
+
+#Adding this because there's an issue not getting what was set up in setup.sh
+source ~/.bashrc
+#Our working directory
+CWD=$(pwd)
+#Get this machine's hostname
+HOSTNAME=$(hostname)
+
+
+KAFKA="$HOSTNAME:9092"
+CLEANSTART=false
+HDFSPATH="hdfs://$HOSTNAME:8020"
+while getopts ":k:h:c" Option
+    # Initial declaration.
+    # a, b, c, d, e, f, and g are the flags expected.
+    # The : after flag 'e' shows it will have an option passed with it.
+    do
+      case $Option in
+        k ) KAFKA=$OPTARG ;;
+        c ) CLEANSTART=true ;;
+        h ) HDFSPATH=$OPTARG ;;
+        * ) usage; exit -1  ;;
+        
+  esac
+done
+
+shift $(( OPTIND - 1 ))
+
 
 #Make sure topics are passed in
 if [ $# = 0 ]
@@ -23,10 +55,6 @@ if [ $# = 0 ]
     exit
 fi
 
-#Adding this because there's an issue not getting what was set up in setup.sh
-source ~/.bashrc
-
-CWD="$(pwd)"
 
 TOPICS=""
 for X in $@
@@ -53,8 +81,15 @@ done
 cd $CWD/Scribengin
 hdfs dfs -mkdir -p /user/yarn/ || true
 hdfs dfs -chown yarn:yarn /user/yarn/ || true
-java -cp $SCRIBEJAR com.neverwinterdp.scribengin.ScribeMaster --topic $TOPICS --broker_list 127.0.0.1:9092 --partition 0  --pre_commit_path_prefix /user/yarn/tmp --commit_path_prefix /user/yarn   --clean_start --hdfsPath hdfs://localhost/ --mode yarn &
-#--hdfsPath hdfs://127.0.0.1:50010 --checkpoint_interval 5000 
+
+CMD="java -cp $SCRIBEJAR com.neverwinterdp.scribengin.ScribeMaster --topic $TOPICS --broker_list $KAFKA --partition 0  --pre_commit_path_prefix /user/yarn/tmp --commit_path_prefix /user/yarn --hdfsPath $HDFSPATH --mode yarn "
+if [ $CLEANSTART = true ] ; then
+    CMD="$CMD --clean_start &"
+else
+    CMD="$CMD &"
+fi
+echo "$CMD"
+eval $CMD
 SCRIBE_PROC_ID=$!
 echo "Scribengin process ID: $SCRIBE_PROC_ID"
 
