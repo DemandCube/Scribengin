b966fb66ca5b9cdd6ac904e0ddecace7488b3984
Tuan Nguyen <tuan08@gmail.com>
2015-02-23 15:59:06 +0700

    fix the broken test

---
+++ V2/release/src/test/java/com/neverwinterdp/scribengin/dataflow/HelloUnitTest.java
@@ -0,0 +1,10 @@
+package com.neverwinterdp.scribengin.dataflow;
+
+import org.junit.Test;
+
+public class HelloUnitTest {
+  @Test
+  public void hello() {
+    System.out.println("This test to make sure the gradle build does not break") ;
+  }
+}
