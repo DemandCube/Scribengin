95cab386c041b5f2e32156c091506a886d7cb249
artfullyContrived <anthony.musyoki@gmail.com>
2015-02-23 10:57:21 +0300

    Converted formatter methods to static inner classes.

--- V2/core/src/main/java/com/neverwinterdp/scribengin/client/shell/Formater.java
+++ V2/core/src/main/java/com/neverwinterdp/scribengin/client/shell/Formater.java
@@ -11,22 +11,26 @@ import com.neverwinterdp.util.text.TabularFormater;
 import com.neverwinterdp.vm.VMDescriptor;
 
 public class Formater {
+
   static public class DataflowList {
-    List<DataflowDescriptor> descriptors ;
-    
+    List<DataflowDescriptor> descriptors;
+
     public DataflowList(List<DataflowDescriptor> descriptors) {
       this.descriptors = descriptors;
     }
-    
-    public String format(String title) { return format(title, ""); }
-    
+
+    public String format(String title) {
+      return format(title, "");
+    }
+
     public String format(String title, String ident) {
-      TabularFormater formater = new TabularFormater("Name", "App Home", "Workers", "Executor Per Worker");
+      TabularFormater formater = new TabularFormater("Name", "App Home", "Workers",
+          "Executor Per Worker");
       formater.setIndent("  ");
-      for(int i = 0; i < descriptors.size(); i++) {
-        DataflowDescriptor descriptor = descriptors.get(i) ;
+      for (int i = 0; i < descriptors.size(); i++) {
+        DataflowDescriptor descriptor = descriptors.get(i);
         formater.addRow(
-            descriptor.getName(), 
+            descriptor.getName(),
             descriptor.getDataflowAppHome(),
             descriptor.getNumberOfWorkers(),
             descriptor.getNumberOfExecutorsPerWorker()
@@ -37,66 +41,115 @@ public class Formater {
     }
   }
 
-  //TODO: 
-  // 1. Check the above formater class, use the inner static class and format method:
-  // 2. Help me to search and replace the other format method in this class
-  public static String format(String title, List<VMDescriptor> vmDescriptors, String leaderPath) {
-    TabularFormater formater = new TabularFormater("name", "CPU Cores", "Memory", "Path", "is Leader");
-    formater.setIndent("  ");
-    for (VMDescriptor descriptor : vmDescriptors) {
-      formater.addRow(descriptor.getVmConfig().getName(), 
-          descriptor.getCpuCores(),
-          descriptor.getMemory(), 
-          descriptor.getStoredPath(), 
-          descriptor.getStoredPath().equals(leaderPath));
+  static public class VmList {
+
+    private List<VMDescriptor> descriptors;
+    String leaderPath;
+
+    public VmList(List<VMDescriptor> descriptors, String leaderPath) {
+      this.descriptors = descriptors;
+      this.leaderPath = leaderPath;
     }
-    formater.setTitle(title);
-    return formater.getFormatText();
+
+    public String format(String title) {
+      return format(title, "");
+    }
+
+    public String format(String title, String ident) {
+      TabularFormater formater = new TabularFormater("name", "CPU Cores", "Memory", "Path",
+          "is Leader");
+      formater.setIndent("  ");
+      for (VMDescriptor descriptor : descriptors) {
+        formater.addRow(descriptor.getVmConfig().getName(),
+            descriptor.getCpuCores(),
+            descriptor.getMemory(),
+            descriptor.getStoredPath(),
+            descriptor.getStoredPath().equals(leaderPath));
+      }
+      formater.setTitle(title);
+      return formater.getFormatText();
+    }
+
   }
-  
-  static public String formatDescriptor(String title, List<DataflowTaskDescriptor> descriptors) {
-    TabularFormater formater = new TabularFormater("Id", "Scribe", "Status");
-    formater.setIndent("  ");
-    for(int i = 0; i < descriptors.size(); i++) {
-      DataflowTaskDescriptor descriptor = descriptors.get(i) ;
-      formater.addRow(
-          descriptor.getId(), 
-          descriptor.getScribe(),
-          descriptor.getStatus()
-      );
+
+  public static class DataFlowTaskDescriptorList {
+
+    private List<DataflowTaskDescriptor> descriptors;
+
+    public DataFlowTaskDescriptorList(List<DataflowTaskDescriptor> descriptors) {
+      this.descriptors = descriptors;
     }
-    formater.setTitle(title);
-    return formater.getFormatText();
+
+    public String format(String title) {
+      return format(title, "");
+    }
+
+    public String format(String title, String ident) {
+      TabularFormater formater = new TabularFormater("Id", "Scribe", "Status");
+      formater.setIndent("  ");
+      for (DataflowTaskDescriptor descriptor : descriptors) {
+        formater.addRow(
+            descriptor.getId(),
+            descriptor.getScribe(),
+            descriptor.getStatus()
+            );
+      }
+      formater.setTitle(title);
+      return formater.getFormatText();
+    }
+
   }
-  
-  static public String formatReport(String title, List<DataflowTaskReport> descriptors, String indent) {
-    TabularFormater formater = new TabularFormater("Process Count", "Commit Count", "Start Time", "Finish Time");
-    formater.setIndent(indent);
-    for(int i = 0; i < descriptors.size(); i++) {
-      DataflowTaskReport descriptor = descriptors.get(i) ;
-      formater.addRow(
-        descriptor.getProcessCount(),
-        descriptor.getCommitProcessCount(),
-        DateUtil.asCompactDateTime(descriptor.getStartTime()),
-        DateUtil.asCompactDateTime(descriptor.getFinishTime())
-      );
+
+  public static class DataflowTaskReportList {
+
+    private List<DataflowTaskReport> descriptors;
+
+    public DataflowTaskReportList(List<DataflowTaskReport> descriptors) {
+      this.descriptors = descriptors;
     }
-    formater.setTitle(title);
-    return formater.getFormatText();
+
+    public String format(String title, String indent) {
+      TabularFormater formater = new TabularFormater("Process Count", "Commit Count", "Start Time",
+          "Finish Time");
+      formater.setIndent(indent);
+      for (DataflowTaskReport descriptor : descriptors) {
+        formater.addRow(
+            descriptor.getProcessCount(),
+            descriptor.getCommitProcessCount(),
+            DateUtil.asCompactDateTime(descriptor.getStartTime()),
+            DateUtil.asCompactDateTime(descriptor.getFinishTime())
+            );
+      }
+      formater.setTitle(title);
+      return formater.getFormatText();
+    }
   }
-  
-  static public String formatExecutor(String title, List<DataflowTaskExecutorDescriptor> descriptors, String indent) {
-    TabularFormater formater = new TabularFormater("Id", "Status", "Assigned Tasks");
-    formater.setIndent(indent);
-    for(int i = 0; i < descriptors.size(); i++) {
-      DataflowTaskExecutorDescriptor descriptor = descriptors.get(i) ;
-      formater.addRow(
-          descriptor.getId(), 
-          descriptor.getStatus(),
-          descriptor.getAssignedTaskIds()
-      );
+
+  public static class ExecutorList {
+
+    private List<DataflowTaskExecutorDescriptor> descriptors;
+
+    public ExecutorList(List<DataflowTaskExecutorDescriptor> descriptors) {
+      this.descriptors = descriptors;
     }
-    formater.setTitle(title);
-    return formater.getFormatText();
+
+    public String format(String title) {
+      return format(title, "");
+    }
+
+    public String format(String title, String indent) {
+      TabularFormater formater = new TabularFormater("Id", "Status", "Assigned Tasks");
+      formater.setIndent(indent);
+      for (DataflowTaskExecutorDescriptor descriptor : descriptors) {
+        formater.addRow(
+            descriptor.getId(),
+            descriptor.getStatus(),
+            descriptor.getAssignedTaskIds()
+            );
+      }
+      formater.setTitle(title);
+      return formater.getFormatText();
+    }
+
   }
 }
--- V2/core/src/test/java/com/neverwinterdp/scribengin/client/shell/DataflowCommandUnitTest.java
+++ V2/core/src/test/java/com/neverwinterdp/scribengin/client/shell/DataflowCommandUnitTest.java
@@ -1,29 +1,25 @@
-package com.neverwinterdp.scribengin;
+package com.neverwinterdp.scribengin.client.shell;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import com.neverwinterdp.scribengin.ScribenginClient;
 import com.neverwinterdp.scribengin.builder.ScribenginClusterBuilder;
-import com.neverwinterdp.scribengin.client.shell.Formater;
-import com.neverwinterdp.scribengin.client.shell.ScribenginShell;
-import com.neverwinterdp.vm.VMDescriptor;
 import com.neverwinterdp.vm.builder.EmbededVMClusterBuilder;
 import com.neverwinterdp.vm.builder.VMClusterBuilder;
 
-public class ScribenginCommandUnitTest {
+public class DataflowCommandUnitTest {
   static {
     System.setProperty("java.net.preferIPv4Stack", "true");
     System.setProperty("log4j.configuration", "file:src/test/resources/test-log4j.properties");
   }
 
-  protected ScribenginClusterBuilder clusterBuilder;
-  protected ScribenginShell shell;
+  protected static ScribenginClusterBuilder clusterBuilder;
+  protected static ScribenginShell shell;
 
   @Before
   public void setup() throws Exception {
@@ -33,11 +29,14 @@ public class ScribenginCommandUnitTest {
     Thread.sleep(3000);
     clusterBuilder.startScribenginMasters();
     shell = new ScribenginShell(clusterBuilder.getVMClusterBuilder().getVMClient());
+
+   
   }
 
   @After
   public void teardown() throws Exception {
     clusterBuilder.shutdown();
+    Thread.sleep(15000);
   }
 
   protected static VMClusterBuilder getVMClusterBuilder() throws Exception {
@@ -47,40 +46,60 @@ public class ScribenginCommandUnitTest {
 
   @Test
   public void testMasterListCommand() throws Exception {
+    DataflowSubmitter submitter = new DataflowSubmitter();
+    submitter.start();
 
-    ScribenginClient scribenginClient = shell.getScribenginClient();
-    assertEquals(2, scribenginClient.getScribenginMasters().size());
-
+    Thread.sleep(15000); // make sure that the dataflow start and running;
     shell.execute("registry dump");
-    shell.execute("scribengin master --list");
-    List<VMDescriptor> vms= scribenginClient.getScribenginMasters();
-    Formater.VmList vmList = new Formater.VmList(vms, "/vm/allocated/vm-scribengin-master-2");
-    String formattedText = vmList.format("Masters");
-    assertTrue(formattedText.contains("vm-scribengin-master-1"));
-    assertTrue(formattedText.contains("/vm/allocated/vm-scribengin-master-2"));
-
+    // shell.execute("dataflow master --list");
   }
 
   @Test
+  @Ignore
   public void testMasterShutdownCommand() throws Exception {
 
     ScribenginClient scribenginClient = shell.getScribenginClient();
     assertEquals(2, scribenginClient.getScribenginMasters().size());
 
-    shell.execute("registry  dump");
-    shell.execute("scribengin master  --shutdown");
+    // shell.execute("registry  dump");
+    shell.execute("dataflow master  --shutdown");
     assertEquals(1, scribenginClient.getScribenginMasters().size());
 
-    shell.execute("registry   dump");
+    // shell.execute("registry  dump");
   }
 
   @Test
+  @Ignore
   public void testMasterInvalidCommand() throws Exception {
 
     ScribenginClient scribenginClient = shell.getScribenginClient();
     assertEquals(2, scribenginClient.getScribenginMasters().size());
 
     // shell.execute("registry   dump");
-    shell.execute("scribengin master  --dummy");
+    shell.execute("dataflow master  --dummy");
+  }
+
+  public class DataflowSubmitter extends Thread {
+    public void run() {
+      try {
+        String command =
+            "dataflow-test kafka "
+                +
+                "  --worker 3 --executor-per-worker 1 --duration 70000 --task-max-execute-time 1000"
+                +
+                "  --kafka-num-partition 10 --kafka-write-period 5 --kafka-max-message-per-partition 3000";
+        shell.execute(command);
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    }
+
+    synchronized void notifyTermimation() {
+      notify();
+    }
+
+    synchronized void waitForTermination(long timeout) throws InterruptedException {
+      wait(timeout);
+    }
   }
 }
