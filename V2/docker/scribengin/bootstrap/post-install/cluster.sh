bbc7b4f8752983efd6aaf361af80dd6aa9801ce4
Peter Jerold Leslie <jeroldleslie@gmail.com>
2015-02-27 01:56:27 -0500

    Start kafka with option

--- V2/docker/scribengin/bootstrap/post-install/cluster.sh
+++ V2/docker/scribengin/bootstrap/post-install/cluster.sh
@@ -260,23 +260,28 @@ function kafka_clean() {
 function kafka_start() {
   #Par the parameters
   clean=false
+  kafka=false
   for i in "$@"; do
     case $i in
       -c|--clean)
       clean=true 
       ;;
+      --with-kafka)
+      kafka=true
+      ;;
       #unknown option
     esac
   done
-
-  #clean the hadoop data and logs if clean = true
-  if  $clean  ; then
-    kafka_clean
+  if  $kafka  ; then
+    #clean the hadoop data and logs if clean = true
+    if  $clean  ; then
+      kafka_clean
+    fi
+ 
+    h1 "Start kafka"
+    servers_exec  "$KAFKA_SERVERS" "/opt/kafka/bin/configure.sh"
+    servers_exec  "$KAFKA_SERVERS" "/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties"
   fi
-
-  h1 "Start kafka"
-  servers_exec  "$KAFKA_SERVERS" "/opt/kafka/bin/configure.sh"
-  servers_exec  "$KAFKA_SERVERS" "/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties"
 }
 
 function kafka_stop() {
@@ -312,7 +317,7 @@ elif [ "$COMMAND" = "sync" ] ; then
   cluster_sync
 elif [ "$COMMAND" = "start" ] ; then
   zookeeper_start $@
-#  kafka_start $@
+  kafka_start $@
   hadoop_start $@
 elif [ "$COMMAND" = "stop" ] ; then
   hadoop_stop $@
