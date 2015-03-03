67950558cc5b2c1cc95cc64cb4ba2c7ff4216e70
Nizar Ellouze <nizarellouze@yahoo.fr>
2015-03-03 13:56:30 +0100

    make kafka producer fails

--- V2/kafka/src/test/java/com/neverwinterdp/kafka/producer/KafkaProducerUnitTest.java
+++ V2/kafka/src/test/java/com/neverwinterdp/kafka/producer/KafkaProducerUnitTest.java
@@ -45,30 +45,41 @@ public class KafkaProducerUnitTest {
     kafkaProps.put("message.send.max.retries", "5");
     kafkaProps.put("retry.backoff.ms", "300");
     
-    KafkaTool kafkaTool = new KafkaTool("test", cluster.getZKConnect());
+    final KafkaTool kafkaTool = new KafkaTool("test", cluster.getZKConnect());
     kafkaTool.connect();
     KafkaWriter writer = new KafkaWriter("test", kafkaProps, cluster.getKafkaConnect());
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10000; i++) {
       writer.send("test", "test-1-" + i);
+      if(i == 10){
+        new Thread(){
+          public void run(){
+
+            try {
+              TopicMetadata topicMeta = kafkaTool.findTopicMetadata("test");
+              PartitionMetadata partitionMeta = topicMeta.partitionsMetadata().get(0);
+              Broker partitionLeader = partitionMeta.leader() ;
+              Server kafkaServer = cluster.findKafkaServerByPort(partitionLeader.port());
+              System.out.println("Shutdown kafka server " + kafkaServer.getPort());
+              kafkaServer.shutdown();
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+           
+          }
+        }.start();
+      }
     }
     System.out.println("Send before leader shutdown");
-    TopicMetadata topicMeta = kafkaTool.findTopicMetadata("test");
-    PartitionMetadata partitionMeta = topicMeta.partitionsMetadata().get(0);
-    Broker partitionLeader = partitionMeta.leader() ;
-    Server kafkaServer = cluster.findKafkaServerByPort(partitionLeader.port());
-    System.out.println("Shutdown kafka server " + kafkaServer.getPort());
-    kafkaServer.shutdown();
-    for(int i = 0; i < 10; i++) {
-      writer.send("test", "test-2-" + i);
-    }
+ 
+
     System.out.println("Send after leader shutdown");
     writer.close();
     kafkaTool.close();
     System.out.println("send done...");
     
     kafkaTool.connect();
-    topicMeta = kafkaTool.findTopicMetadata("test");
-    partitionMeta = topicMeta.partitionsMetadata().get(0);
+    TopicMetadata topicMeta = kafkaTool.findTopicMetadata("test");
+    PartitionMetadata partitionMeta = topicMeta.partitionsMetadata().get(0);
     KafkaPartitionReader partitionReader = new KafkaPartitionReader("test", "test", partitionMeta);
     List<byte[]> messages = partitionReader.fetch(10000, 30);
     for(int i = 0; i < messages.size(); i++) {
@@ -81,3 +92,4 @@ public class KafkaProducerUnitTest {
     kafkaTool.close();
   }
 }
+
