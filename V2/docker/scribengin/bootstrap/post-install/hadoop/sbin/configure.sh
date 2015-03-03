0fa8bb32c5d8945c15dedb16e939f092a9d37d88
Peter Jerold Leslie <jeroldleslie@gmail.com>
2015-03-03 07:21:53 -0500

    optional number of servers for zookeeper, kafka, hadoop worker

---
+++ V2/docker/scribengin/bootstrap/post-install/hadoop/sbin/configure.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+HOSTNAME=$(hostname)
+
+
+HADOOP_SERVERS="";
+HADOOP_MASTER_SERVERS="";
+HADOOP_WORKER_SERVERS="";
+ZOOKEEPER_SERVERS="";
+KAFKA_SERVERS="";
+ALL_SERVERS="";
+
+#Parse /etc/hosts file to get the cluster hostname
+function parse_hosts_file() {
+  FILENAME="/etc/hosts"
+  while read LINE
+  do
+    if [[ $LINE ==  $'## scribengin server ##'* ]]
+    then
+     BEGIN_SCRIBENGIN_SERVER=TRUE
+     continue
+    elif [[ $LINE == \#* ]]  #Ignore the comment line
+    then
+      continue
+    elif [[ $LINE == "" ]]  #Ignore the empty line
+    then
+      continue
+    fi
+
+    if [[ -n "$BEGIN_SCRIBENGIN_SERVER" ]] #Check not null, empty BEGIN_SCRIBENGIN_SERVER
+    then
+      arrLine=(${LINE// / }) #split line that contains '$IP $HOSTNAME' format
+      hostname=${arrLine[1]}
+
+      if [[ $hostname ==  $'hadoop'* ]]
+      then
+        HADOOP_SERVERS="$HADOOP_SERVERS $hostname"
+        if [[ $hostname ==  $'hadoop-master'* ]]; then
+          HADOOP_MASTER_SERVERS="$HADOOP_MASTER_SERVERS $hostname"
+        elif [[ $hostname ==  $'hadoop-worker'* ]]; then
+          HADOOP_WORKER_SERVERS="$HADOOP_WORKER_SERVERS $hostname"
+        fi
+      elif [[ $hostname ==  $'zookeeper'* ]]
+      then
+        ZOOKEEPER_SERVERS="$ZOOKEEPER_SERVERS $hostname"
+      elif [[ $hostname ==  $'kafka'* ]]
+      then
+        KAFKA_SERVERS="$KAFKA_SERVERS $hostname"
+      fi
+    fi
+  done < $FILENAME
+
+  ALL_SERVERS="$HADOOP_SERVERS $ZOOKEEPER_SERVERS $KAFKA_SERVERS"
+
+}
+function create_slave_file() {
+  for WORKER in $HADOOP_WORKER_SERVERS; do
+    WORKERS+="$WORKER"
+    WORKERS+=$'\n'
+  done
+  echo "$WORKERS"  > /opt/hadoop/etc/hadoop/slaves
+}
+
+parse_hosts_file
+create_slave_file
