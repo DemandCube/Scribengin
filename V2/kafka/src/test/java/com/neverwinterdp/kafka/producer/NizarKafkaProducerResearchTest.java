9d44462dfd5d35e052d174b9ddd504440e57a1b1
Tuan Nguyen <tuan08@gmail.com>
2015-03-05 21:36:11 +0700

    Reproduce the kafka failure as Steve describe

--- V2/kafka/src/test/java/com/neverwinterdp/kafka/producer/NizarKafkaProducerResearchTest.java
+++ V2/kafka/src/test/java/com/neverwinterdp/kafka/producer/NizarKafkaProducerResearchTest.java
@@ -42,36 +42,44 @@ public class NizarKafkaProducerResearchTest {
   public void testKafkaProducer() throws Exception {
     Map<String, String> kafkaProps = new HashMap<String, String>();
     kafkaProps.put("message.send.max.retries", "5");
-    kafkaProps.put("retry.backoff.ms", "1000");
+    kafkaProps.put("retry.backoff.ms", "100");
+    kafkaProps.put("queue.buffering.max.ms", "1000");
+    kafkaProps.put("queue.buffering.max.messages", "5000");
+    kafkaProps.put("request.required.acks", "-1");
+    kafkaProps.put("topic.metadata.refresh.interval.ms", "-1"); //negative value will refresh on failure
+    kafkaProps.put("batch.num.messages", "10");
+    kafkaProps.put("producer.type", "sync");
     
     final KafkaTool kafkaTool = new KafkaTool("test", cluster.getZKConnect());
     kafkaTool.connect();
     KafkaWriter writer = new KafkaWriter("test", kafkaProps, cluster.getKafkaConnect());
+    Runnable kafkaKiller = new Runnable() {
+      public void run() {
+        try {
+          TopicMetadata topicMeta = kafkaTool.findTopicMetadata("test");
+          PartitionMetadata partitionMeta = topicMeta.partitionsMetadata().get(0);
+          Broker partitionLeader = partitionMeta.leader() ;
+          Server kafkaServer = cluster.findKafkaServerByPort(partitionLeader.port());
+          System.out.println("Shutdown kafka server " + kafkaServer.getPort());
+          kafkaServer.shutdown();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    };
     //send 10 000 messages
     for(int i = 0; i < 10000; i++) {
-      writer.send("test", "test-1-" + i);
+      writer.send("test", 0, "key-" + i, "test-1-" + i);
       //after sending 10 messages we shutdown and continue sending
       //this will make the producer fails after sending about 30 messages
       if(i == 10){
-        new Thread() {
-          public void run() {
-            try {
-              TopicMetadata topicMeta = kafkaTool.findTopicMetadata("test");
-              PartitionMetadata partitionMeta = topicMeta.partitionsMetadata().get(0);
-              Broker partitionLeader = partitionMeta.leader() ;
-              Server kafkaServer = cluster.findKafkaServerByPort(partitionLeader.port());
-              System.out.println("Shutdown kafka server " + kafkaServer.getPort());
-              kafkaServer.shutdown();
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
-          }
-        }.start();
+        new Thread(kafkaKiller).start();
+        //kafkaKiller.run();
       }
     }
     System.out.println("Send before leader shutdown");
  
-
+    Thread.sleep(3000);
     System.out.println("Send after leader shutdown");
     writer.close();
     kafkaTool.close();
