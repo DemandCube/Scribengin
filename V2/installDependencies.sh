7d0f33c9c38148b1992aca5ee6d5c2baee67a11f
KingOfPoptart <richard.c.duarte@gmail.com>
2015-03-10 10:53:35 +0000

    ensuring pull from master

--- V2/installDependencies.sh
+++ V2/installDependencies.sh
@@ -1,14 +1,14 @@
 git clone https://github.com/DemandCube/Queuengin/
 git clone https://github.com/DemandCube/NeverwinterDP-Commons
 cd Queuengin
-git pull
+git pull origin master
 ../../gradlew clean build install -x test
 cd ../NeverwinterDP-Commons
-git pull
+git pull origin master
 ../../gradlew clean build install -x test
 cd ../
 cd jvmagent/registry
-git pull
+git pull origin master
 ../../../gradlew clean build release
 cd ../../
 ../gradlew eclipse
