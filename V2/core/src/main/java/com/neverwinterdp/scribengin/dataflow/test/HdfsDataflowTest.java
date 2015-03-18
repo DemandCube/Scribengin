caa5de91f1b473d224f98533cf008ea5f9fde959
Nizar Ellouze <nizarellouze@yahoo.fr>
2015-03-18 17:23:32 +0100

    hdfs to hdfs dataflow

--- V2/core/src/main/java/com/neverwinterdp/scribengin/dataflow/test/HdfsDataflowTest.java
+++ V2/core/src/main/java/com/neverwinterdp/scribengin/dataflow/test/HdfsDataflowTest.java
@@ -2,22 +2,10 @@ package com.neverwinterdp.scribengin.dataflow.test;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 
-import com.beust.jcommander.Parameter;
-import com.neverwinterdp.kafka.tool.KafkaTool;
-import com.neverwinterdp.registry.RegistryConfig;
-import com.neverwinterdp.scribengin.ScribenginClient;
 import com.neverwinterdp.scribengin.client.shell.ScribenginShell;
-import com.neverwinterdp.scribengin.dataflow.DataflowDescriptor;
-import com.neverwinterdp.scribengin.dataflow.test.HelloKafkaDataflowBuilder.TestCopyScribe;
 import com.neverwinterdp.scribengin.event.ScribenginWaitingEventListener;
 import com.neverwinterdp.scribengin.hdfs.HDFSSourceGenerator;
-import com.neverwinterdp.scribengin.kafka.KafkaSourceGenerator;
-import com.neverwinterdp.scribengin.sink.SinkDescriptor;
-import com.neverwinterdp.scribengin.source.SourceDescriptor;
-import com.neverwinterdp.vm.HadoopProperties;
-import com.neverwinterdp.vm.environment.yarn.HDFSUtil;
 
 
 public class HdfsDataflowTest extends DataflowTest {
