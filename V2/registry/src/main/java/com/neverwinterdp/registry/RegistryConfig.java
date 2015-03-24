b61d2dec69d66490bab09d591ac1c4e1dbb53300
Nizar Ellouze <nizarellouze@yahoo.fr>
2015-03-24 18:43:00 +0100

    update from dev/master

--- V2/registry/src/main/java/com/neverwinterdp/registry/RegistryConfig.java
+++ V2/registry/src/main/java/com/neverwinterdp/registry/RegistryConfig.java
@@ -2,16 +2,22 @@ package com.neverwinterdp.registry;
 
 import com.beust.jcommander.Parameter;
 import com.google.inject.Singleton;
+import com.mycila.jmx.annotation.JmxBean;
+import com.mycila.jmx.annotation.JmxField;
 import com.neverwinterdp.registry.zk.RegistryImpl;
 
 @Singleton
+@JmxBean("role=RegistryConfig, type=RegistryConfig, name=RegistryConfig")
 public class RegistryConfig {
+  @JmxField
   @Parameter(names = "--registry-connect", description = "The registry connect string")
   private String connect ;
   
+  @JmxField
   @Parameter(names = "--registry-db-domain", description = "The registry partition or table")
   private String dbDomain ;
   
+  @JmxField
   @Parameter(names = "--registry-implementation", description = "The registry implementation class")
   private String registryImplementation ;
   
