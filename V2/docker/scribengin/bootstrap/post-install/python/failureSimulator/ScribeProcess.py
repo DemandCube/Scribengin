150149b7514135580266bbf64d0dfb3bd8da1b14
KingOfPoptart <richard.c.duarte@gmail.com>
2015-03-27 14:25:11 +0000

    Fixing zk process finding bug

--- V2/docker/scribengin/bootstrap/post-install/python/failureSimulator/ScribeProcess.py
+++ V2/docker/scribengin/bootstrap/post-install/python/failureSimulator/ScribeProcess.py
@@ -6,14 +6,14 @@ class ScribeProcess:
     self.scribeServerHelper = scribeServerHelper
   
   def getRunningKafkaProcessIDs(self):
-    return self.getRunningProcessIDs("kafka")
+    return self.getRunningProcessIDs("kafka", "kafka")
   
   def getRunningZookeeperProcessIDs(self):
-    return self.getRunningProcessIDs("QuorumPeerMain")
+    return self.getRunningProcessIDs("QuorumPeerMain", "zookeeper")
   
-  def getRunningProcessIDs(self, procName):
+  def getRunningProcessIDs(self, procName, hostnameRegex):
     command = "ps ax | grep -i '"+procName+"' | grep java | grep -v grep | awk '{print $1}'"
-    output = self.scribeServerHelper.execServer(command, "kafka")
+    output = self.scribeServerHelper.execServer(command, hostnameRegex)
     result = []
     for o in output:
       result.append(output[o]["stdout"].strip())
