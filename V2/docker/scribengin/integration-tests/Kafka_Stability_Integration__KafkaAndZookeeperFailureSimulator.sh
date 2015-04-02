3f7eb4b352b6bfbd31f0e44b7e92668af41a88c5
artfullyContrived <anthony.musyoki@gmail.com>
2015-04-02 18:54:39 +0300

    Correctly pick up the xml files from docker.

--- V2/docker/scribengin/integration-tests/Kafka_Stability_Integration__KafkaAndZookeeperFailureSimulator.sh
+++ V2/docker/scribengin/integration-tests/Kafka_Stability_Integration__KafkaAndZookeeperFailureSimulator.sh
@@ -27,12 +27,12 @@ ssh -f -n -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/scrib
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin/tools/kafka/ && ./kafka.sh test stability --zk-connect zookeeper-1:2181 --topic stabilitytest --replication 2 --send-period 0 --send-writer-type ack --send-max-duration 1800000 --send-max-per-partition 10000000 --producer:message.send.max.retries=5 --producer:retry.backoff.ms=100 --producer:queue.buffering.max.ms=1000 --producer:queue.buffering.max.messages=15000 --producer:topic.metadata.refresh.interval.ms=-1 --producer:batch.num.messages=100 --producer:acks=all --consume-max 50000000 --consume-max-duration 5550000 --junit-report KafkaMessageCheckTool.xml"
 
 #Get results
-scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/scribengin/scribengin/tools/kafka/KafkaMessageCheckTool.xml ./
+scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/scribengin/scribengin/tools/kafka/KafkaMessageCheckTool*.xml ./
 
 #Allow failure simulator to save test results
 sleep 10
 
-#Get results for failure simulatore
+#Get results for failure simulator
 scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/scribengin/scribengin/tools/kafka/results/FAILURE_TEST_failure_simulator.xml ./
 
 #Clean up
