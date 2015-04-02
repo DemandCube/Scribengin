f475ff764d60b97ef467760e644397fe72670b04
Tuan Nguyen <tuan08@gmail.com>
2015-04-02 17:02:58 +0700

    update test config

--- V2/docker/scribengin/integration-tests/Kafka_Stability_Integration__KafkaFailureSimulator.sh
+++ V2/docker/scribengin/integration-tests/Kafka_Stability_Integration__KafkaFailureSimulator.sh
@@ -19,7 +19,7 @@ sleep 20
 
 
 #Run failure simulator in the background
-ssh -f -n -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin/tools/kafka/  && nohup ./failure_simulator.sh simulate --kafka-broker=kafka-1,kafka-2,kafka-3 --wait-before-start=10 --kafka-failure=10 --min-kafka=2"
+ssh -f -n -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin/tools/kafka/  && nohup ./failure_simulator.sh simulate --kafka-broker=kafka-1,kafka-2,kafka-3,kafka-4 --wait-before-start=10 --kafka-failure=10 --min-kafka=3"
 
 #Run kafkaStabilityCheckTool
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin/tools/kafka/ && ./kafka.sh test stability --zk-connect zookeeper-1:2181 --topic stabilitytest --replication 3 --send-period 0 --send-writer-type ack --send-max-duration 1800000 --send-max-per-partition 20000000 --producer:message.send.max.retries=5 --producer:retry.backoff.ms=100 --producer:queue.buffering.max.ms=1000 --producer:queue.buffering.max.messages=15000 --producer:topic.metadata.refresh.interval.ms=-1 --producer:batch.num.messages=100 --producer:acks=all --producer:compression.type=gzip --consume-max 100000000 --consume-max-duration 6000000 --junit-report KafkaMessageCheckTool.xml"
