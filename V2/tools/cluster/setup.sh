ce7fc9bd62f26e7c025e7c9f331cb27d7d5cdb23
KingOfPoptart <richard.c.duarte@gmail.com>
2015-04-03 17:22:34 +0100

    Fixing setup script

--- V2/tools/cluster/setup.sh
+++ V2/tools/cluster/setup.sh
@@ -11,5 +11,12 @@ fi
 #Setup easy_install
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 
+#The previous step runs in the background on some machines, so loop until its done
+while [ "`which easy_install`" == "" ] ; do
+  sleep 1
+done
+
 #Install required modules
 sudo easy_install nose==1.3.4 tabulate paramiko junit-xml click
+
+echo "Setup complete!"
