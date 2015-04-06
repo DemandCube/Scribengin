b71b54c90f3c12b58258b3b6f73fbbd3d6472123
artfullyContrived <anthony.musyoki@gmail.com>
2015-04-06 11:05:33 +0300

    First commit for HDFS source and sink implementations.

---
+++ V2/core/src/main/java/com/neverwinterdp/scribengin/dataflow/test/hdfs/HDFSReport.java
@@ -0,0 +1,52 @@
+package com.neverwinterdp.scribengin.dataflow.test.hdfs;
+
+public class HDFSReport {
+
+  private WriterReport writerReport;
+  private ReaderReport readerReport;
+
+  public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public WriterReport getWriterReport() {
+    return writerReport;
+  }
+
+  public ReaderReport getReaderReport() {
+    return readerReport;
+  }
+
+  public int getNumOfStreams() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+   class WriterReport {
+
+    public int getMessageSent() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    public long getRunDuration() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+  }
+
+   class ReaderReport {
+
+    public int getReadCount() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    public long getRunDuration() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+  }
+}
