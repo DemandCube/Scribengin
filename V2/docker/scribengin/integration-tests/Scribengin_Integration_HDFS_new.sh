281d13dace969bed167b06f20d94603d42488f75
KingOfPoptart <richard.c.duarte@gmail.com>
2015-04-13 10:54:19 +0100

    Fixing integration tests for Jenkins

--- V2/docker/scribengin/integration-tests/Scribengin_Integration_HDFS_new.sh
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_HDFS_new.sh
@@ -12,13 +12,13 @@ sudo ./docker.sh update-hosts
 #Start cluster
 ######ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt && ./cluster.sh kafka start --with-zookeeper"
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt && ./cluster.sh start --with-kafka --clean"
+sleep 5
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh vm start"
+sleep 5
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh scribengin start"
+sleep 5
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh scribengin info"
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh vm info"
-
-#Give everything time to come up
-sleep 20
 
 #Run dataflow
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh dataflow-test hdfs --worker 3 --executor-per-worker 1 --duration 10000 --task-max-execute-time 1000 --source-num-of-stream 10 --source-max-records-per-stream 1000 --source-dataflowName hello-source --sink-dataflowName hello-source --junit-report HDFS_IntegrationTest.xml"
--- V2/docker/scribengin/integration-tests/Scribengin_Integration_Kafka.sh
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_Kafka.sh
@@ -10,15 +10,14 @@ cd docker/scribengin/
 sudo ./docker.sh update-hosts
 
 #Start cluster
-######ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt && ./cluster.sh kafka start --with-zookeeper"
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt && ./cluster.sh start --with-kafka --clean"
+sleep 5
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh vm start"
+sleep 5
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh scribengin start"
+sleep 5
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh scribengin info"
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh vm info"
-
-#Give everything time to come up
-sleep 20
 
 
 
