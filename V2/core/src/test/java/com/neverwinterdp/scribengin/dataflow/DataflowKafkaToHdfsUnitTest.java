9460368a62e18979339a0c14a28fc05b8433120e
artfullyContrived <anthony.musyoki@gmail.com>
2015-04-16 10:22:57 +0300

    Increase KafkaToHDFS unit tests messages.

--- V2/core/src/test/java/com/neverwinterdp/scribengin/dataflow/DataflowKafkaToHdfsUnitTest.java
+++ V2/core/src/test/java/com/neverwinterdp/scribengin/dataflow/DataflowKafkaToHdfsUnitTest.java
@@ -74,9 +74,9 @@ public class DataflowKafkaToHdfsUnitTest {
             " --duration 90000" +
             " --task-max-execute-time 10000" +
             " --source-name output" +
-            " --source-num-of-stream 5" +
+            " --source-num-of-stream 10" +
             " --source-write-period 5" +
-            " --source-max-records-per-stream 1000" +
+            " --source-max-records-per-stream 10000" +
             " --sink-name output" +
             " --print-dataflow-info -1" +
             " --debug-dataflow-task true" +
