51558da8336c29a47f5fe5ed1844d13a758979f4
artfullyContrived <anthony.musyoki@gmail.com>
2015-04-17 11:29:16 +0300

    Improvements for HDFS intergration tests.

---
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_HDFS_To_HDFS.sh
@@ -0,0 +1,28 @@
+#Set up docker images
+DOCKERSCRIBEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+$DOCKERSCRIBEDIR/../startCluster.sh
+
+#make folder for test results
+mkdir testresults
+
+#Start cluster
+ssh -o StrictHostKeyChecking=no neverwinterdp@hadoop-master "cd /opt/cluster && python clusterCommander.py cluster --start --clean status"
+
+sleep 5
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh scribengin info"
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh vm info"
+
+#Run dataflow
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && \
+                              ./bin/shell.sh dataflow-test hdfs --dataflow-name  hdfs   \
+                              --worker 3 --executor-per-worker 1 --duration 90000 --task-max-execute-time 10000 \
+                              --source-name input --source-num-of-stream 10 --source-write-period 5 \
+                              --source-max-records-per-stream 100000 --sink-name input  --print-dataflow-info -1 \
+                              --debug-dataflow-task true --debug-dataflow-worker true \
+                              --junit-report HDFS_IntegrationTest.xml --dump-registry"
+
+#Get results
+scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/scribengin/scribengin/HDFS_IntegrationTest.xml ./testresults/
+
+#Clean up
+$DOCKERSCRIBEDIR/../stopCluster.sh
