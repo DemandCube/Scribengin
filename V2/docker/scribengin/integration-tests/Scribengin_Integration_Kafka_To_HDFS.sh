246960d0ce0d3caf5f8c83bb0bae8e723d4de5f3
artfullyContrived <anthony.musyoki@gmail.com>
2015-04-17 14:21:15 +0300

    Increases run duration for KafkaToHDFS intergraton test.

--- V2/docker/scribengin/integration-tests/Scribengin_Integration_Kafka_To_HDFS.sh
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_Kafka_To_HDFS.sh
@@ -16,7 +16,7 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengi
 #Run dataflow
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && \
                               ./bin/shell.sh dataflow-test kafka-hdfs --dataflow-name kafka-hdfs   \
-                              --worker 3 --executor-per-worker 1 --duration 90000 --task-max-execute-time 10000 \
+                              --worker 3 --executor-per-worker 1 --duration 100000 --task-max-execute-time 50000 \
                               --source-name input --source-num-of-stream 10 --source-write-period 5 \
                               --source-max-records-per-stream 100000 --sink-name input  --print-dataflow-info -1 \
                               --debug-dataflow-task true --debug-dataflow-worker true \
