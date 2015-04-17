efeb9f4af50986c3b959f8f2b5c8bc4b86f9509d
artfullyContrived <anthony.musyoki@gmail.com>
2015-04-17 13:43:05 +0300

    Renames the HDFSToKafka script.

--- V2/docker/scribengin/integration-tests/Scribengin_Integration_HDFS_To_Kafka.sh
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_HDFS_To_Kafka.sh
@@ -14,7 +14,13 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengi
 
 
 #Run dataflow
-ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && ./bin/shell.sh  dataflow-test hdfs-kafka --worker 3 --executor-per-worker 1 --duration 70000 --task-max-execute-time 1000 --source-name input --source-num-of-stream 10 --source-write-period 5 --source-max-records-per-stream 3000 --sink-name output --junit-report HDFS_to_Kafka_IntegrationTest.xml"
+ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && \
+                              ./bin/shell.sh dataflow-test hdfs-kafka --dataflow-name hdfs-kafka   \
+                              --worker 3 --executor-per-worker 1 --duration 90000 --task-max-execute-time 10000 \
+                              --source-name input --source-num-of-stream 10 --source-write-period 5 \
+                              --source-max-records-per-stream 100000 --sink-name input  --print-dataflow-info -1 \
+                              --debug-dataflow-task true --debug-dataflow-worker true \
+                              --junit-report HDFS_to_Kafka_IntegrationTest.xml --dump-registry"
 
 #Get results
 scp -o stricthostkeychecking=no neverwinterdp@hadoop-master:/opt/scribengin/scribengin/HDFS_to_Kafka_IntegrationTest.xml ./testresults/
