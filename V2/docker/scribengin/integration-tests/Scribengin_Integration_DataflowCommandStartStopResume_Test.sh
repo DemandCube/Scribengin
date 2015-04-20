87f0c59af51da2c68344b86fff733b4cca0a80d9
Richard Duarte <richard.c.duarte@gmail.com>
2015-04-20 22:42:05 +0100

    Update Scribengin_Integration_DataflowCommandStartStopResume_Test.sh

--- V2/docker/scribengin/integration-tests/Scribengin_Integration_DataflowCommandStartStopResume_Test.sh
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_DataflowCommandStartStopResume_Test.sh
@@ -14,14 +14,15 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengi
 
 #Run start/stop/resume
 ssh -f -n -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master \ 
-  "cd /opt/scribengin/scribengin && \
+  "mkdir -p /opt/junit-reports/ && cd /opt/scribengin/scribengin && \
   nohup ./bin/shell.sh dataflow-test start-stop-resume --wait-before-start 25000 --sleep-before-execute 10000 \
      --max-wait-for-stop  20000 --max-wait-for-resume  20000  --print-summary \
-     --junit-report /opt/junit-reports/DataflowTestStartStopResume.xml"
+     --junit-report /opt/junit-reports/DataflowTestStartStopResume.xml \
+     && echo $! > /opt/junit-reports/save_pid.txt"
 
 #Run dataflow
 ssh  -o StrictHostKeyChecking=no neverwinterdp@hadoop-master \
-  "mkdir -p /opt/junit-reports/ && cd /opt/scribengin/scribengin && \
+   "cd /opt/scribengin/scribengin && \
    ./bin/shell.sh dataflow-test kafka-to-kakfa  --dataflow-name  kafka-to-kafka --worker 2 \
      --executor-per-worker 2 --duration 300000 --task-max-execute-time 5000 \
      --source-name input --source-num-of-stream 10 --source-write-period 0  \
@@ -31,6 +32,8 @@ ssh  -o StrictHostKeyChecking=no neverwinterdp@hadoop-master \
 
 #Print the running processes
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "/opt/cluster/clusterCommander.py status"
+
+wait `ssh`
 
 sleep 30
 #Get results
