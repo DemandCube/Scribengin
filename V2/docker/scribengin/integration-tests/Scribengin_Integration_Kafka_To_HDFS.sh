7bd8ae29ec3deabf6187edc1c3221b24f81ca378
KingOfPoptart <richard.c.duarte@gmail.com>
2015-04-28 18:16:52 +0100

    Fixing dataflow-test name

--- V2/docker/scribengin/integration-tests/Scribengin_Integration_Kafka_To_HDFS.sh
+++ V2/docker/scribengin/integration-tests/Scribengin_Integration_Kafka_To_HDFS.sh
@@ -15,7 +15,7 @@ ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengi
 
 #Run dataflow
 ssh -o "StrictHostKeyChecking no" neverwinterdp@hadoop-master "cd /opt/scribengin/scribengin && \
-                              ./bin/shell.sh dataflow-test kafka-hdfs --dataflow-name kafka-hdfs   \
+                              ./bin/shell.sh dataflow-test kafka-to-hdfs --dataflow-name kafka-hdfs   \
                               --worker 3 --executor-per-worker 1 --duration 100000 --task-max-execute-time 50000 \
                               --source-name input --source-num-of-stream 10 --source-write-period 5 \
                               --source-max-records-per-stream 100000 --sink-name input  --print-dataflow-info -1 \
