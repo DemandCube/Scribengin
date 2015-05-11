5a5853fcc7f43f448ffc77a4db8381c3124c471a
Tuan Nguyen <tuan08@gmail.com>
2015-05-11 19:40:13 +0700

    UI TODO for Anthonny or Richard

--- V2/client/swingui/src/main/java/com/neverwinterdp/swing/scribengin/dataflow/UIDataflowDescriptorView.java
+++ V2/client/swingui/src/main/java/com/neverwinterdp/swing/scribengin/dataflow/UIDataflowDescriptorView.java
@@ -2,6 +2,7 @@ package com.neverwinterdp.swing.scribengin.dataflow;
 
 import javax.swing.JPanel;
 
+//TODO: Implement this UI using the SpringLayoutGridJPanel, @see UIActivityView
 @SuppressWarnings("serial")
 public class UIDataflowDescriptorView extends JPanel {
   private String dataflowId ;
--- V2/client/swingui/src/main/java/com/neverwinterdp/swing/scribengin/dataflow/UIDataflowWorkerView.java
+++ V2/client/swingui/src/main/java/com/neverwinterdp/swing/scribengin/dataflow/UIDataflowWorkerView.java
@@ -2,6 +2,7 @@ package com.neverwinterdp.swing.scribengin.dataflow;
 
 import javax.swing.JPanel;
 
+//TODO: Implement this UI using the JXTable.  @see UIActivityListView to see how to create and populate an JXTable
 @SuppressWarnings("serial")
 public class UIDataflowWorkerView extends JPanel {
   private String dataflowId ;
