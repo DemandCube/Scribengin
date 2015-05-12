c86b25211f47a8b53e9a172246b71c60bd976968
Tuan Nguyen <tuan08@gmail.com>
2015-05-12 21:07:45 +0700

    add registry logger and log code to try to figure out the response lost problem

---
+++ V2/registry/src/main/java/com/neverwinterdp/registry/RegistryLogger.java
@@ -0,0 +1,44 @@
+package com.neverwinterdp.registry;
+
+
+public class RegistryLogger {
+  private Node     logNode ;
+  
+  public RegistryLogger(Registry registry, String path) throws RegistryException {
+    logNode = registry.createIfNotExist(path) ;
+  }
+  
+  public void info(String name, String mesg) throws RegistryException {
+    Log log = new Log("INFO", mesg) ;
+    logNode.createChild("INFO-" + name, log, NodeCreateMode.PERSISTENT_SEQUENTIAL);
+  }
+  
+  public void error(String name, String mesg) throws RegistryException {
+    Log log = new Log("INFO", mesg) ;
+    logNode.createChild("INFO-" + name, log, NodeCreateMode.PERSISTENT_SEQUENTIAL);
+  }
+  
+  static public class Log {
+    private long   timestamp ;
+    private String level ;
+    private String message ;
+    
+    public Log() {}
+    
+    public Log(String level, String message) {
+      this.timestamp = System.currentTimeMillis() ;
+      this.level = level ;
+      this.message = message ;
+    }
+
+    public long getTimestamp() { return timestamp; }
+    public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
+
+    public String getLevel() { return level; }
+    public void setLevel(String level) { this.level = level; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+  }
+}
+
