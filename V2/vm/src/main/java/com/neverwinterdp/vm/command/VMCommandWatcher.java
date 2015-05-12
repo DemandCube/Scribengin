39177cc44d630b796ee91da6db7cf57a9f52fc25
Tuan Nguyen <tuan08@gmail.com>
2015-05-12 16:39:07 +0700

    attempt to fix the lost response when server is killed

--- V2/vm/src/main/java/com/neverwinterdp/vm/command/VMCommandWatcher.java
+++ V2/vm/src/main/java/com/neverwinterdp/vm/command/VMCommandWatcher.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.neverwinterdp.registry.event.NodeEvent;
 import com.neverwinterdp.registry.event.NodeWatcher;
 import com.neverwinterdp.registry.Registry;
+import com.neverwinterdp.util.ExceptionUtil;
 import com.neverwinterdp.vm.VM;
 
 public class VMCommandWatcher extends NodeWatcher {
@@ -14,8 +15,6 @@ public class VMCommandWatcher extends NodeWatcher {
     this.vm = vm ;
   }
   
-  private long seqTracker = 0 ;
-  
   @Override
   synchronized public void onEvent(NodeEvent event) {
     if(NodeEvent.Type.CHILDREN_CHANGED == event.getType()) {
@@ -24,19 +23,19 @@ public class VMCommandWatcher extends NodeWatcher {
         Registry registry = vm.getVMRegistry().getRegistry() ;
         registry.watchChildren(path, this);
         List<String> cmdPathNames = registry.getChildren(path, true);
-        //System.out.println("path = " + path + ", children = " + cmdPathNames.size());
         for(String cmdPathName : cmdPathNames) {
-          String seq = cmdPathName.substring("command-".length()) ;
-          long seqId = Long.parseLong(seq);
-          //System.out.println("  cmdPathName " + cmdPathName);
-          if(seqId == seqTracker++) {
-            String cmdPath = path + "/" + cmdPathName;
-            CommandPayload payload = registry.getDataAs(cmdPath, CommandPayload.class);
-            Command command = payload.getCommand();
-            CommandResult<?> result = command.execute(vm);
-            payload.setResult(result);
-            registry.setData(cmdPath, payload);
+          String cmdPath = path + "/" + cmdPathName;
+          CommandPayload payload = registry.getDataAs(cmdPath, CommandPayload.class);
+          Command command = payload.getCommand();
+          CommandResult<?> result = null ;
+          try {
+            result = command.execute(vm);
+          } catch(Throwable t) {
+            result = new CommandResult() ;
+            result.setErrorStacktrace(ExceptionUtil.getStackTrace(t));
           }
+          payload.setResult(result);
+          registry.setData(cmdPath, payload);
         }
       } catch (Throwable e) {
         e.printStackTrace();
