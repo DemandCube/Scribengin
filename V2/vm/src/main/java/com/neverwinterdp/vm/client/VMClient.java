8d2dadb5fcfd9715c62d0ea81d8f3e68fed14ebd
Tuan Nguyen <tuan08@gmail.com>
2015-05-12 15:01:00 +0700

    fix build fail

--- V2/vm/src/main/java/com/neverwinterdp/vm/client/VMClient.java
+++ V2/vm/src/main/java/com/neverwinterdp/vm/client/VMClient.java
@@ -71,7 +71,7 @@ public class VMClient {
   public CommandResult<?> execute(VMDescriptor vmDescriptor, Command command, long timeout) throws Exception {
     CommandPayload payload = new CommandPayload(command, null) ;
     Node node = registry.create(vmDescriptor.getRegistryPath() + "/commands/command-", payload, NodeCreateMode.EPHEMERAL_SEQUENTIAL);
-    CommandReponseWatcher responseWatcher = new CommandReponseWatcher(registry, node.getPath());
+    CommandReponseWatcher responseWatcher = new CommandReponseWatcher(registry, node.getPath(), command);
     node.watchModify(responseWatcher);
     return responseWatcher.waitForResult(timeout);
   }
