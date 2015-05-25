3603daae1977ad4cb8d33917cc5e6aa5a385f35e
Tuan Nguyen <tuan08@gmail.com>
2015-05-25 11:01:01 -0400

    installDependencies.sh should be removed

--- V2/installDependencies.sh
+++ V2/installDependencies.sh
@@ -1,14 +1,15 @@
-git clone https://github.com/DemandCube/Queuengin/
-git clone https://github.com/DemandCube/NeverwinterDP-Commons
-cd Queuengin
-git pull origin master
-../../gradlew clean build install -x test
-cd ../NeverwinterDP-Commons
-git pull origin master
-../../gradlew clean build install -x test
-cd ../
-cd jvmagent/registry
-git pull origin master
-../../../gradlew clean build release
-cd ../../
-../gradlew eclipse
+#git clone https://github.com/DemandCube/Queuengin/
+#git clone https://github.com/DemandCube/NeverwinterDP-Commons
+#cd Queuengin
+#git pull origin master
+#../../gradlew clean build install -x test
+#cd ../NeverwinterDP-Commons
+#git pull origin master
+#../../gradlew clean build install -x test
+#cd ../
+#cd jvmagent/registry
+#git pull origin master
+#../../../gradlew clean build release
+#cd ../../
+#../gradlew eclipse
+echo "TODO: remove this file"
