9fcfa0d122875a264b56af915b012818044806ad
Tuan Nguyen <tuan08@gmail.com>
2015-05-28 15:45:56 +0700

    reorganize code , move commons to module/commons, release to scribengin/release

--- V2/registry/vm/src/main/java/com/neverwinterdp/vm/environment/yarn/MiniClusterUtil.java
+++ V2/registry/vm/src/main/java/com/neverwinterdp/vm/environment/yarn/MiniClusterUtil.java
@@ -5,7 +5,6 @@ import java.io.File;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
-import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hdfs.MiniDFSNNTopology;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
